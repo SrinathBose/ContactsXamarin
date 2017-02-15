2628c587d85dbc413a2dc8114b5dd1851884a7ce
SrinathBose <ssrinathbose@gmail.com>
2017-02-15 18:47:34 +0530

    Add files via upload

---
+++ Contact/SaveContact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Contact.IOS;
+using Foundation;
+using Newtonsoft.Json;
+
+[assembly:Xamarin.Forms.Dependency(typeof(SaveContact))]
+
+namespace Contact.IOS
+{
+	public class SaveContact : IContactPreferences
+	{
+        public void SaveContactToPreferences(Person newContact , string key)
+		{
+            var jsonData = JsonConvert.SerializeObject(newContact);
+            //NSUserDefaults.StandardUserDefaults.Se
+            NSUserDefaults.StandardUserDefaults.SetString(jsonData,key);
+   		}
+       
+        public Person GetContact(string key)
+        {
+            var retrievedValue = NSUserDefaults.StandardUserDefaults.StringForKey(key);
+            Console.WriteLine(" Retrieved Value Is = {0} ", retrievedValue);
+
+            Person newPerson = JsonConvert.DeserializeObject<Person>(retrievedValue);
+
+            Console.WriteLine("Name Is {0} ", newPerson.Name);
+            Console.WriteLine("Number Is {0} ", newPerson.Number);
+            Console.WriteLine("Address Is {0} ", newPerson.Address);
+
+            return newPerson;
+        }
+	}
+}
